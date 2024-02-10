e6809d603a5915e83a531cb1278327f202e46b9a
KernelInterrupt <254471942@qq.com>
2024-02-11 02:10:43 +0800

    "add text detection"

--- example/video_player_object_detection/lib/object_detection.dart
+++ example/video_player_object_detection/lib/object_detection.dart
@@ -19,6 +19,203 @@ import 'dart:ui' as ui;
 import 'package:flutter/services.dart';
 import 'package:image/image.dart' as img;
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'dart:math' as math;
+import 'package:dart_tensor/dart_tensor.dart';
+import 'package:vector_math/vector_math.dart' as vm;
+
+class ImageAnalysisResult {
+  final String type;
+  final List<int> classes;
+  final List<String> classication;
+  final List<double> scores;
+  final List<List<double>> locations;
+  final int numberOfDetections;
+  final int originalHeight;
+  final int originalWidth;
+  final int scaledHeight;
+  final int scaledWidth;
+  
+  
+  
+  ImageAnalysisResult({
+    required this.type,
+    required this.classes,
+    required this.classication,
+    required this.scores,
+    required this.locations,
+    required this.numberOfDetections,
+    required this.originalHeight,
+    required this.originalWidth,
+    required this.scaledHeight,
+    required this.scaledWidth
+  });
+}
+
+class TextDetection {
+  static const String _modelPath = 'assets/models/east_model_float32.tflite';
+  
+
+  Interpreter? _interpreter;
+ // declaration of DartTensor class
+
+DartTensor dt = DartTensor();
+  TextDetection() {
+    _loadModel();
+    
+    log('Done.');
+  }
+
+  Future<void> _loadModel() async {
+    log('Loading interpreter options...(Text)');
+    final interpreterOptions = InterpreterOptions();
+
+    // Use XNNPACK Delegate
+    if (Platform.isAndroid) {
+      interpreterOptions.addDelegate(XNNPackDelegate());
+    }
+
+    // Use Metal Delegate
+    if (Platform.isIOS) {
+      interpreterOptions.addDelegate(GpuDelegate());
+    }
+
+    log('Loading interpreter...');
+    _interpreter =
+        await Interpreter.fromAsset(_modelPath, options: interpreterOptions);
+  }
+
+ 
+
+  ImageAnalysisResult analyseImageUI(Uint8List imageData) {
+    log('Analysing image...');
+    // Reading image bytes from file
+
+    // Decoding image
+    final image = img.decodeImage(imageData);
+    final int height=image!.height;
+    final int width=image!.width;
+    // Resizing image fpr model, [300, 300]
+    final imageInput = img.copyResize(
+      image!,
+      width: 320,
+      height: 320,
+    );
+
+    // Creating matrix representation, [300, 300, 3]
+    final imageMatrix = List.generate(
+  imageInput.height,
+  (y) => List.generate(
+    imageInput.width,
+    (x) {
+      final pixel = imageInput.getPixel(x, y);
+      return [pixel.r.toDouble(), pixel.g.toDouble(), pixel.b.toDouble()];
+    },
+  ),
+);
+
+  
+
+  
+    final output = _runInference(imageMatrix);
+    int lastNativeInferenceDuration =
+        (_interpreter!.lastNativeInferenceDurationMicroSeconds / 1000).round();
+    log('lastNativeInferenceDuration time: $lastNativeInferenceDuration ms');
+    log('Processing outputs...');
+    // Location
+        // Scores
+    final scores = output.first as List<List<List<List<double>>>>;
+    log('Scores: $scores');
+
+
+    // Location
+    final locationsRaw = output.elementAt(1) as List<List<List<List<double>>>>;
+    var numRows = scores[0][0].length;
+var numCols = scores[0][0][0].length;
+
+
+List<List<double>> locations=[[]];
+var numberOfDetections=0;
+// Loop over the number of rows
+for (int y = 0; y < numRows; y++) {
+    // Assuming locationsRaw is also a List of Lists that can be indexed similarly to your Python code
+    List<double> scoresData = scores[0][0][y];
+    List<double> xData0 = locationsRaw[0][0][y];
+    List<double> xData1 = locationsRaw[0][1][y];
+    List<double> xData2 = locationsRaw[0][2][y];
+    List<double> xData3 = locationsRaw[0][3][y];
+    List<double> anglesData = locationsRaw[0][4][y];
+    
+    // Loop over the number of columns
+    for (int x = 0; x < numCols; x++) {
+        // If our score does not have sufficient probability, ignore it
+        if (scoresData[x] < 0.5) {
+            continue;
+        }
+        locations.add([]);
+        // Compute the offset factor as our resulting feature maps will
+        // be 4x smaller than the input image
+        double offsetX = x * 4.0;
+        double offsetY = y * 4.0;
+        
+        // Extract the rotation angle for the prediction and then
+        // compute the sin and cosine
+        double angle = anglesData[x];
+        double cos = math.cos(angle);
+        double sin = math.sin(angle);
+        
+        double h = xData0[x] + xData2[x];
+        double w = xData1[x] + xData3[x];
+        var endX = (offsetX + (cos * xData1[x]) + (sin * xData2[x])) ;
+          var endY = (offsetY - (sin * xData1[x]) + (cos * xData2[x])) ;
+            var startX = (endX - w) ;
+            var startY =(endY - h) ;
+            locations[numberOfDetections].addAll([startY,startX,endY,endX]);
+            
+            numberOfDetections++;
+
+    }
+}
+    
+     return ImageAnalysisResult(
+      
+      type:'Text',
+      classes: [],
+      classication: [],
+      scores:[],
+      locations: locations,
+      numberOfDetections: numberOfDetections,
+      originalHeight:height,
+      originalWidth:width,
+      scaledHeight: 320,
+      scaledWidth: 320
+      
+    );
+  }
+    
+  
+
+  List<List<Object>> _runInference(
+    List<List<List<num>>> imageMatrix,
+  ) {
+    log('Running inference...');
+
+    // Set input tensor [1, 320, 320, 3]
+    final input = [imageMatrix];
+
+    
+    final output = {
+      0: [List.generate(1, (_) =>List.generate(80, (_) =>List<double>.filled(80, 0)))],
+      1: [List.generate(5, (_) =>List.generate(80, (_) =>List<double>.filled(80, 0)))],
+      
+    };
+
+    _interpreter!.runForMultipleInputs([input], output);
+    return output.values.toList();
+  }
+}
+
+
+
 
 class ObjectDetection {
   static const String _modelPath = 'assets/models/ssd_mobilenet.tflite';
@@ -34,7 +231,7 @@ class ObjectDetection {
   }
 
   Future<void> _loadModel() async {
-    log('Loading interpreter options...');
+    log('Loading interpreter options...(Object)');
     final interpreterOptions = InterpreterOptions();
 
     // Use XNNPACK Delegate
@@ -59,13 +256,14 @@ class ObjectDetection {
   }
 
 
-  Uint8List analyseImageUI(Uint8List imageData) {
+  ImageAnalysisResult analyseImageUI(Uint8List imageData) {
     log('Analysing image...');
     // Reading image bytes from file
 
     // Decoding image
     final image = img.decodeImage(imageData);
-
+    final int height=image!.height;
+    final int width=image!.width;
     // Resizing image fpr model, [300, 300]
     final imageInput = img.copyResize(
       image!,
@@ -84,7 +282,7 @@ class ObjectDetection {
         },
       ),
     );
-
+  
     final output = _runInference(imageMatrix);
     int lastNativeInferenceDuration =
         (_interpreter!.lastNativeInferenceDurationMicroSeconds / 1000).round();
@@ -93,7 +291,7 @@ class ObjectDetection {
     // Location
     final locationsRaw = output.first.first as List<List<double>>;
     final locations = locationsRaw.map((list) {
-      return list.map((value) => (value * 300).toInt()).toList();
+      return list.map((value) => (value * 300).toDouble()).toList();
     }).toList();
     log('Locations: $locations');
 
@@ -118,35 +316,27 @@ class ObjectDetection {
     }
 
     log('Outlining objects...');
-    for (var i = 0; i < numberOfDetections; i++) {
-      if (scores[i] > 0.6) {
-        // Rectangle drawing
-        img.drawRect(
-          imageInput,
-          x1: locations[i][1],
-          y1: locations[i][0],
-          x2: locations[i][3],
-          y2: locations[i][2],
-          color: img.ColorRgb8(255, 0, 0),
-          thickness: 3,
-        );
-
-        // Label drawing
-        img.drawString(
-          imageInput,
-          '${classication[i]} ${scores[i]}',
-          font: img.arial14,
-          x: locations[i][1] + 1,
-          y: locations[i][0] + 1,
-          color: img.ColorRgb8(255, 0, 0),
-        );
-      }
-    }
+    
 
     log('Done.');
 
-    return img.encodeJpg(imageInput);
+    
+     return ImageAnalysisResult(
+      type:'Object',
+      classes: classes,
+      classication:classication,
+      scores: scores,
+      locations: locations,
+      numberOfDetections: numberOfDetections,
+      originalHeight:height,
+      originalWidth:width,
+      scaledHeight: 300,
+      scaledWidth: 300
+      
+    );
   }
+    
+  
 
   Uint8List analyseImage(String imagePath) {
     log('Analysing image...');
@@ -260,3 +450,4 @@ class ObjectDetection {
     return output.values.toList();
   }
 }
+
--- example/video_player_object_detection/lib/video_utils.dart
+++ example/video_player_object_detection/lib/video_utils.dart
@@ -4,12 +4,14 @@ import 'dart:ui' as ui;
 import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+
 import 'dart:io';
 import 'package:video_player/video_player.dart';
 import 'package:fvp/fvp.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:object_detection_ssd_mobilenet/object_detection.dart';
 import 'package:image/image.dart' as img;
+
 class Video_utils extends StatefulWidget {
   const Video_utils({super.key});
 
@@ -17,56 +19,57 @@ class Video_utils extends StatefulWidget {
   State<Video_utils> createState() => video_utils();
 }
 
-class video_utils extends State<Video_utils>{
-
-
-
-final imagePicker = ImagePicker();
-late VideoPlayerController controller;
+class video_utils extends State<Video_utils> {
+  final imagePicker = ImagePicker();
+  late VideoPlayerController controller;
   late File videoFile;
   String? videoPath;
   String? videoUrl;
-
+  TextDetection? textDetection;
   ObjectDetection? objectDetection;
-GlobalKey _containerKey=GlobalKey();
+
+  GlobalKey _containerKey = GlobalKey();
   Uint8List? image;
 
   @override
   void initState() {
     super.initState();
     objectDetection = ObjectDetection();
+    textDetection=TextDetection();
     controller = VideoPlayerController.networkUrl(Uri.parse(""));
     controller.initialize().then((_) {
       setState(() {});
     });
   }
+
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
   }
-Future<void> selectVideo() async {
-  FilePickerResult? result = await FilePicker.platform.pickFiles(
-    type: FileType.video,
-    allowMultiple: false,
-  );
-  if (result != null) {
-    setState(() {
-      videoFile = File(result.files.single.path!);
-      videoPath = videoFile.path;
-      videoUrl = null; // 清空视频URL
-    });
-    controller.dispose();
-    setState(() {});
-    controller = VideoPlayerController.file(videoFile);
-    controller.initialize().then((_) {
-      controller.play();
+
+  Future<void> selectVideo() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.video,
+      allowMultiple: false,
+    );
+    if (result != null) {
+      setState(() {
+        videoFile = File(result.files.single.path!);
+        videoPath = videoFile.path;
+        videoUrl = null; // 清空视频URL
+      });
+      controller.dispose();
       setState(() {});
-      
-    });
+      controller = VideoPlayerController.file(videoFile);
+      controller.initialize().then((_) {
+        controller.play();
+        setState(() {});
+      });
+    }
   }
-}
- void playVideoFromUrl() {
+
+  void playVideoFromUrl() {
     if (videoUrl != null) {
       setState(() {
         videoPath = null; // 清空视频路径
@@ -83,155 +86,266 @@ Future<void> selectVideo() async {
   Widget buildVideoWidget() {
     if (controller.value.isInitialized) {
       return RepaintBoundary(
-        key:_containerKey,
-        child:AspectRatio(
-        aspectRatio: controller.value.aspectRatio,
-        child: VideoPlayer(controller),
-      ),
+        key: _containerKey,
+        child: AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: VideoPlayer(controller),
+        ),
       );
     } else {
       return const Text('No video selected');
     }
   }
 
-
-Future<ui.Image> captureImageFromVideo(GlobalKey _containerKey) async {
-    RenderRepaintBoundary boundary =
-    _containerKey.currentContext?.findRenderObject() as RenderRepaintBoundary;
+  Future<ui.Image> captureImageFromVideo(GlobalKey _containerKey) async {
+    RenderRepaintBoundary boundary = _containerKey.currentContext
+        ?.findRenderObject() as RenderRepaintBoundary;
     ui.Image capturedImage =
         await boundary.toImage(pixelRatio: ui.window.devicePixelRatio);
     return capturedImage;
   }
+
 //ui.Image转Uint8List
- Future<Uint8List> uiImage2Uint8List(ui.Image image) async{
-  ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-  final bytes = byteData!.buffer.asInt8List();
-  final bytesBuffer=bytes.buffer;
+  Future<Uint8List> uiImage2Uint8List(ui.Image image) async {
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    final bytes = byteData!.buffer.asInt8List();
+    final bytesBuffer = bytes.buffer;
     return Uint8List.view(bytesBuffer);
   }
+ img.Image drawQuadrilateral(img.Image image,List<List<int>> coordinate)
+ {
+    // 连接四边形的四个顶点
+    for (int i = 0; i < coordinate.length; i++) {
+      //x1=coordinate[i][1];
+      //y1=coordinate[i][0];
+      //x2=coordinate[i][3];
+      //y2=coordinate[i][2];
+      img.drawLine(image,
+            x1: coordinate[i][1],
+            y1: coordinate[i][0],
+            x2: coordinate[i][3],
+            y2: coordinate[i][2],
+            color: img.ColorRgb8(255, 0, 0),
+            thickness: 3,);
+    }
+    
+  
+  return image;
+
+ }
+  Uint8List drawResult(Uint8List imageData, ImageAnalysisResult result) {
+    final imageTmp = img.decodeImage(imageData);
+    final imageForDrawing = imageTmp!;
+    String type = result.type;
+    List<int> classes = result.classes;
+    List<List<double>> locations = result.locations;
+    List<String> classication = result.classication;
+    List<double> scores = result.scores;
+    int numberOfDetections = result.numberOfDetections;
+    int originalHeight = result.originalHeight;
+    int originalWidth = result.originalWidth;
+    int scaledHeight = result.scaledHeight;
+    int scaledWidth = result.scaledWidth;
+    
+      
+    for (int i = 0; i < numberOfDetections; i++) {
+      for (int j = 0; j < locations[i].length; j++) {
+        if (j == 1 || j == 3) {
+          // 转换x轴坐标
+          locations[i][j] =
+              locations[i][j] * originalWidth / scaledWidth;
+        } else if (i == 0 && j == 2) {
+          // 执行第二行第二列元素的操作
+          locations[i][j] =
+              locations[i][j] * originalHeight / scaledHeight;
+        } else {
+          // 其他元素保持不变
+        }
+      }
+    }
+    List<List<int>> finalLocations = locations.map((row) {
+      return row.map((element) {
+        return element.toInt();
+      }).toList();
+    }).toList();
+    if (type == 'Object') {
+      for (var i = 0; i < numberOfDetections; i++) {
+        if (scores[i] > 0.6) {
+          // Rectangle drawing
+          img.drawRect(
+            imageForDrawing,
+            x1: finalLocations[i][1],
+            y1: finalLocations[i][0],
+            x2: finalLocations[i][3],
+            y2: finalLocations[i][2],
+            color: img.ColorRgb8(255, 0, 0),
+            thickness: 3,
+          );
+
+          // Label drawing
+          img.drawString(
+            imageForDrawing,
+            '${classication[i]} ${scores[i]}',
+            font: img.arial14,
+            x: finalLocations[i][1] + 1,
+            y: finalLocations[i][0] + 1,
+            color: img.ColorRgb8(255, 0, 0),
+          );
+        }
+      }
+    }
+    else if(type=='Text')//文字类score在数据后处理时已经根据阈值筛选了，所以这里不需要按照score（置信度）来筛选画图
+    {
+        for (var i = 0; i < numberOfDetections; i++) {
+        
+          // Rectangle drawing
+          img.drawRect(
+            imageForDrawing,
+            x1: finalLocations[i][1],
+            y1: finalLocations[i][0],
+            x2: finalLocations[i][3],
+            y2: finalLocations[i][2],
+            color: img.ColorRgb8(255, 0, 0),
+            thickness: 3,
+          );
+
+          
+        
+      }
+    }
+
+    return img.encodePng(imageForDrawing);
+  }
+
 //暂时没用，保存截图到某个路径
   Future<void> saveImage(ui.Image image) async {
-  ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-  final bytes = byteData!.buffer.asInt8List().cast<int>();
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    final bytes = byteData!.buffer.asInt8List().cast<int>();
 
-  // 指定保存目录的路径
-  final directory = Directory('/home/pc/images'); // 将路径替换为想要保存到的目录路径
+    // 指定保存目录的路径
+    final directory = Directory('/home/pc/images'); // 将路径替换为想要保存到的目录路径
 
-  if (await directory.exists()) {
-    // 生成一个唯一的文件名
-    final fileName = 'video_snapshot.png';
+    if (await directory.exists()) {
+      // 生成一个唯一的文件名
+      final fileName = 'video_snapshot.png';
 
-    // 创建文件并写入截图数据
-    final file = File('${directory.path}/$fileName');
-    await file.writeAsBytes(bytes);
-    print('Screenshot saved at: ${file.path}');
-  } else {
-    print('目录不存在：${directory.path}');
+      // 创建文件并写入截图数据
+      final file = File('${directory.path}/$fileName');
+      await file.writeAsBytes(bytes);
+      print('Screenshot saved at: ${file.path}');
+    } else {
+      print('目录不存在：${directory.path}');
+    }
   }
-}
-void PlayVideo(){
-  controller.play();
-  setState(() {});
-}
-@override
+
+  void PlayVideo() {
+    controller.play();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    
-    
-        return SafeArea(
-         
-            
-            child:SizedBox(
-              
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
+    return SafeArea(
+      child: SizedBox(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
               child: Center(
                 child: (image != null) ? Image.memory(image!) : Container(),
               ),
             ),
-                  Expanded(
-                  child:buildVideoWidget(),
-                  ),
-           const SizedBox(),
+            Expanded(
+              child: buildVideoWidget(),
+            ),
+            const SizedBox(),
             if (videoPath == null)
               Expanded(
-                child:TextField(
-                onChanged: (value) {
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      videoUrl = value.trim();
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Enter video URL',
+                  ),
+                ),
+              ),
+            if (videoPath != null)
+              FloatingActionButton(
+                onPressed: () {
+                  // Wrap the play or pause in a call to `setState`. This ensures the
+                  // correct icon is shown.
                   setState(() {
-                    videoUrl = value.trim();
+                    // If the video is playing, pause it.
+                    if (controller.value.isPlaying) {
+                      controller.pause();
+                    } else {
+                      // If the video is paused, play it.
+                      controller.play();
+                    }
                   });
                 },
-                decoration: const InputDecoration(
-                  labelText: 'Enter video URL',
+                // Display the correct icon depending on the state of the player.
+                child: Icon(
+                  controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
                 ),
               ),
+            Positioned(
+              bottom: 20,
+              right: 20,
+              child: ElevatedButton(
+                onPressed: () async {
+                  ui.Image capturedImage =
+                      await captureImageFromVideo(_containerKey);
+                  
+                  Uint8List imageTmp = await uiImage2Uint8List(capturedImage);
+                  ImageAnalysisResult result =
+                      textDetection!.analyseImageUI(imageTmp);
+                  image = drawResult(imageTmp, result);
+
+                  setState(() {});
+                },
+                child: const Text('Analyze Text'),
               ),
-            if(videoPath!=null)
-              FloatingActionButton(
-              onPressed: () {
-                 // Wrap the play or pause in a call to `setState`. This ensures the
-                  // correct icon is shown.
-                setState(() {
-      // If the video is playing, pause it.
-                if (controller.value.isPlaying) {
-                    controller.pause();
-                  } else {
-        // If the video is paused, play it.
-                controller.play();
-                }
-              });
-            },
-  // Display the correct icon depending on the state of the player.
-        child: Icon(
-          controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-          ),
-        ),
-        Positioned(
-                bottom: 20,
-                right: 20,
-                child: ElevatedButton(
-                  onPressed: () async{
-                    ui.Image capturedImage= await captureImageFromVideo(_containerKey);
-                    saveImage(capturedImage);
-                      Uint8List imageTmp=await uiImage2Uint8List(capturedImage);
-                        image = objectDetection!.analyseImageUI(imageTmp);
+            ),
+            Positioned(
+              bottom: 20,
+              right: 20,
+              child: ElevatedButton(
+                onPressed: () async {
+                  ui.Image capturedImage =
+                      await captureImageFromVideo(_containerKey);
+                  saveImage(capturedImage);
+                  Uint8List imageTmp = await uiImage2Uint8List(capturedImage);
+                  ImageAnalysisResult result =
+                      objectDetection!.analyseImageUI(imageTmp);
+                  image = drawResult(imageTmp, result);
 
-                    
-                         setState(() {});
-                  },
-                  child: const Text('Analyze'),
-                ),
+                  setState(() {});
+                },
+                child: const Text('Analyze'),
               ),
-
-
+            ),
             const SizedBox(height: 16.0),
             if (videoPath == null && videoUrl == null)
               ElevatedButton(
                 onPressed: selectVideo,
-                child:const Expanded(
-
-                 child:Text('Select Video Files'),
+                child: const Expanded(
+                  child: Text('Select Video Files'),
                 ),
-            ),
-
+              ),
             if (videoPath == null && videoUrl != null)
               ElevatedButton(
                 onPressed: playVideoFromUrl,
-                child:const Expanded(
-
-                 child:Text('Play Video'),
+                child: const Expanded(
+                  child: Text('Play Video'),
                 ),
               ),
-                ],
-              ),
-            ),
-            
-        
-        );
-      
-    
+          ],
+        ),
+      ),
+    );
   }
-  
-  
 }
